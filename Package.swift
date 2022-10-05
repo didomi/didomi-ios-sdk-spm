c0123ea28a2fbe40c9403aac5a2ab3551ceb6836
Felipe Saez <felipe.saez.victoriano@gmail.com>
2022-10-05 12:40:42 -0300

    Removed comments from Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Didomi",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Didomi",
-            targets: ["Didomi"]),
+            targets: ["Didomi"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Didomi",
             url: "https://sdk.didomi.io/ios/didomi-ios-sdk-1.74.0-xcframework.zip",
