4b5c7d6399e48a1b272166cf99f1ac4363bb4000
Didomi CMP Team <cmp-mobile@didomi.io>
2024-05-10 16:33:40 +0200

    Version: updated version to 2.6.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Didomi",
-            url: "https://sdk.didomi.io/ios/didomi-ios-sdk-2.5.0-xcframework.zip",
-            checksum: "9bcfd7ada6eb6b37a34aa13b8f78567b5b362d843732066d1eae0cf2e63486b0"
+            url: "https://sdk.didomi.io/ios/didomi-ios-sdk-2.6.0-xcframework.zip",
+            checksum: "0b78eb653f07ddc708c919679e94a2c3ded8a34850c5f02f07f729cca42a067c"
         )
     ]
 )
